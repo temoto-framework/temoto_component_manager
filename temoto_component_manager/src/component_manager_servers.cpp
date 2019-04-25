a7d2d60dd774fa07656b895f04ba4b8e5753c781
RValner <valnerrobert@gmail.com>
2019-04-25 20:19:22 +0300

    Removed the temporary parameter hack

--- temoto_component_manager/src/component_manager_servers.cpp
+++ temoto_component_manager/src/component_manager_servers.cpp
@@ -517,9 +517,9 @@ void ComponentManagerServers::loadPipeCb(LoadPipe::Request& req, LoadPipe::Respo
         {
           if (seg_param_spec.segment_index == i)
           {
-            load_component_msg.request.required_parameters.push_back(seg_param_spec.parameters[0]);
+            load_component_msg.request.required_parameters = seg_param_spec.parameters;
             load_component_msg.request.component_name = seg_param_spec.component_name;
-            //break;
+            break;
           }
         }
 
