eb21fcf4d335d098ff6e6bb8fefb10b28f29a25a
Robert <valnerrobert@gmail.com>
2021-04-13 19:48:34 +0300

    load component operator defined

--- temoto_component_manager/include/temoto_component_manager/component_manager_services.h
+++ temoto_component_manager/include/temoto_component_manager/component_manager_services.h
@@ -41,67 +41,147 @@ namespace temoto_component_manager
   }
 }
 
-/**
- * @brief operator ==
- * @param r1
- * @param r2
- * @return
- */
 static bool operator==(const temoto_component_manager::LoadComponent::Request& r1,
-                       const temoto_component_manager::LoadComponent::Request& r2)
+                       const temoto_component_manager::LoadComponent& r2)
 {
-  // First check the basics
-  if (r1.component_type != r2.component_type ||
-      r1.package_name != r2.package_name ||
-      r1.executable != r2.executable)
+  /*
+   * This operator assumes that the response portion of previous queries (r2) includes
+   * all the details regarding the component that was provided per request. Thus ambiguous
+   * queries can be compared against previous queries.
+   */ 
+
+  if (r1.component_type != r2.request.component_type)
   {
     return false;
   }
 
-  // Check the size of the requested output topics
-  if (r1.output_topics.size() != r2.output_topics.size())
+  if (!r1.package_name.empty() && r1.package_name != r2.response.package_name)
   {
     return false;
   }
 
-  // Check the requested output topics
-  auto output_topics_2_copy = r2.output_topics;
-  for (auto& output_topic_1 : r1.output_topics)
+  if (!r1.executable.empty() && r1.executable != r2.response.executable)
+  {
+    return false;
+  }
+
+  /*
+   * Check the input topics
+   */ 
+  if (r1.input_topics.size() > r2.response.input_topics.size())
+  {
+    return false;
+  }
+
+  auto r2_input_topics_copy = r2.response.input_topics;
+  for(const auto& r1_input_topic : r1.input_topics)
   {
     bool topic_found = false;
-    for (auto it=output_topics_2_copy.begin(); it!=output_topics_2_copy.end(); it++)
+    for (auto it=r2_input_topics_copy.begin(); it!=r2_input_topics_copy.end(); it++)
     {
-      if (output_topic_1.key == it->key &&
-          output_topic_1.value == it->value)
+      if (r1_input_topic.key == it->key && r1_input_topic.value.empty())
       {
         topic_found = true;
-        output_topics_2_copy.erase(it);
+        r2_input_topics_copy.erase(it);
         break;
       }
+      else if (r1_input_topic.key == it->key && !r1_input_topic.value.empty())
+      {
+        // If the keys are equal but the topics are different this request is unique
+        if (r1_input_topic.value == it->value)
+        {
+          topic_found = true;
+          r2_input_topics_copy.erase(it);
+          break;
+        }
+        else
+        {
+          return false;
+        }
+      }
     }
-
     if (!topic_found)
     {
       return false;
     }
   }
 
-  // The component infos are equal
+  /*
+   * Check the output topics
+   */ 
+  if (r1.output_topics.size() > r2.response.output_topics.size())
+  {
+    return false;
+  }
+
+  auto r2_output_topics_copy = r2.response.output_topics;
+  for(const auto& r1_output_topic : r1.output_topics)
+  {
+    bool topic_found = false;
+    for (auto it=r2_output_topics_copy.begin(); it!=r2_output_topics_copy.end(); it++)
+    {
+      if (r1_output_topic.key == it->key && r1_output_topic.value.empty())
+      {
+        topic_found = true;
+        r2_output_topics_copy.erase(it);
+        break;
+      }
+      else if (r1_output_topic.key == it->key && !r1_output_topic.value.empty())
+      {
+        // If the keys are equal but the topics are different this request is unique
+        if (r1_output_topic.value == it->value)
+        {
+          topic_found = true;
+          r2_output_topics_copy.erase(it);
+          break;
+        }
+        else
+        {
+          return false;
+        }
+      }
+    }
+    if (!topic_found)
+    {
+      return false;
+    }
+  }
+
+  /*
+   * Check the parameters
+   */
+  if (r1.required_parameters.size() > r2.response.required_parameters.size())
+  {
+    return false;
+  }
+
+  auto r2_parameters_copy = r2.response.required_parameters;
+  for(const auto& r1_parameter : r1.required_parameters)
+  {
+    bool parameter_found = false;
+    for (auto it=r2_parameters_copy.begin(); it!=r2_parameters_copy.end(); it++)
+    {
+      if (r1_parameter.key == it->key && r1_parameter.value == it->value)
+      {
+        parameter_found = true;
+        r2_parameters_copy.erase(it);
+        break;
+      }
+    }
+    if (!parameter_found)
+    {
+      return false;
+    }
+  }
+
+  // The component requests are equal
   return true;
 }
 
-/**
- * @brief 
- * 
- * @param r1 
- * @param r2 
- * @return true 
- * @return false 
- */
 static bool operator==(const temoto_component_manager::LoadPipe::Request& r1,
-                       const temoto_component_manager::LoadPipe::Request& r2)
+                       const temoto_component_manager::LoadPipe& r2)
 {
-  if (r1.pipe_category != r2.pipe_category)
+  if (r1.pipe_category != r2.request.pipe_category)
   {
     return false;
   }
@@ -112,14 +192,14 @@ static bool operator==(const temoto_component_manager::LoadPipe::Request& r1,
   for (const auto& output_topic_r1 : r1.output_topics)
   {
     auto found = std::find_if(
-      r2.output_topics.begin(),
-      r2.output_topics.end(),
+      r2.request.output_topics.begin(),
+      r2.request.output_topics.end(),
       [&] (const diagnostic_msgs::KeyValue& t2)
     { 
       return ((output_topic_r1.key == t2.key) && (output_topic_r1.value == t2.value));
     });
 
-    if (found == r2.output_topics.end())
+    if (found == r2.request.output_topics.end())
     {
       return false;
     }
@@ -131,7 +211,7 @@ static bool operator==(const temoto_component_manager::LoadPipe::Request& r1,
   for (const auto& segment_specifier_r1 : r1.pipe_segment_specifiers)
   {
     bool segment_specifier_found = false;
-    for (const auto& segment_specifier_r2 : r2.pipe_segment_specifiers)
+    for (const auto& segment_specifier_r2 : r2.request.pipe_segment_specifiers)
     {
       // compare the segment index
       if (segment_specifier_r1.segment_index != segment_specifier_r2.segment_index)
